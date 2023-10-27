7425050d5aedcd62058a2b46dd4df7147091d0e7
ansel brandt <ansel@anselbrandt.com>
2023-10-27 13:07:00 -0400

    Remove unused props

--- src/components/PinMap.tsx
+++ src/components/PinMap.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import ReactMap, {
-  FullscreenControl,
-  GeolocateControl,
-  MapRef,
-  NavigationControl,
-  ViewState,
-} from "react-map-gl";
+import ReactMap, { MapRef, ViewState } from "react-map-gl";
 import type { Properties } from "../data";
-import { Geocoder } from "./Geocoder";
 import { Pins } from "./Pins";
-import { CircleLayer } from "./layers/CircleLayer";
-import { DrawPolygon } from "./draw/DrawPolygon";
-import { PolygonLayer } from "./layers/PolygonLayer";
-import { DrawCircle } from "./draw/DrawCircle";
 
 interface MapProps {
   width: number | string;
@@ -20,11 +9,6 @@ interface MapProps {
   data: GeoJSON.FeatureCollection<GeoJSON.Point, Properties>;
   initialViewState: Partial<ViewState>;
   mapRef: React.RefObject<MapRef>;
-  theme: string;
-  drawMode: string;
-  setFeatures: (obj: any) => void;
-  geofenceType: string;
-  radius: number;
 }
 
 export const Map: React.FC<MapProps> = ({
@@ -33,16 +17,8 @@ export const Map: React.FC<MapProps> = ({
   data,
   initialViewState,
   mapRef,
-  theme,
-  drawMode,
-  setFeatures,
-  geofenceType,
-  radius,
 }) => {
-  const mapStyle =
-    theme === "dark"
-      ? "mapbox://styles/mapbox/dark-v11"
-      : "mapbox://styles/mapbox/light-v11";
+  const mapStyle = "mapbox://styles/mapbox/light-v11";
 
   return (
     <ReactMap
@@ -52,17 +28,7 @@ export const Map: React.FC<MapProps> = ({
       style={{ width, height }}
       mapStyle={mapStyle}
     >
-      <Geocoder position="top-left" />
-      <GeolocateControl position="top-left" />
-      <FullscreenControl position="top-left" />
-      <NavigationControl position="top-left" />
       <Pins data={data} />
-      {drawMode === "polygon" ? (
-        <DrawPolygon setFeatures={setFeatures} />
-      ) : null}
-      {drawMode === "circle" ? <DrawCircle setFeatures={setFeatures} /> : null}
-      {geofenceType === "circle" ? <CircleLayer radius={radius} /> : null}
-      {geofenceType === "polygon" ? <PolygonLayer /> : null}
     </ReactMap>
   );
 };
